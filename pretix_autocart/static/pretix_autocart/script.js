0b1dcbc0705249b7a5c3e6d8d0925997960f0eb2
Luca Sorace "Stranck <strdjn@gmail.com>
2024-09-24 20:45:30 +0200

    Everything should be working
    I still need to figure out where the cp_XX and id_XX for addons and questions comes from

--- pretix_autocart/static/pretix_autocart/script.js
+++ pretix_autocart/static/pretix_autocart/script.js
@@ -2,7 +2,7 @@
 function setCookie(key, value, expiry) {
 	var expires = new Date();
 	expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
-	document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+	document.cookie = key + '=' + value + ';path=/;expires=' + expires.toUTCString();
 }
 function getCookie(key) {
 	var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
@@ -21,6 +21,14 @@ $(document).ready(function(){
 		var signature = urlParams.get('s');
 		var isDataInUrl = true;
 		
+		/*var shit0 = atob(action);
+		var shit = "-----BEGIN RSA PRIVATE KEY----- MIICXQIBAAKBgQCsy9dch/xC2hVi0pHsyEdgeuVf2yupb2dEl3IMpKdnLrBc8yd8W1XBs3754EzGNwffaAm3dhwOhnohaXRsnzrjSkyzd2k4t/H5YOsn/kKx4eDmE0PQyfcO1GyXB26x8Y++2AJsvBEvLmm7sLZVQejH+xYmoJrDFuU7LgF5YRmOKwIDAQABAoGAAYw12ptRNBwV8vOl4PQOR5vyMACbjIH179RXbiuPYEo3xC1dXizHQNvluEE9Ds1xYZvh8mW5bJvhvijepRZsBZdTbvpsh1ky6bZu8N/RpFumCc05IK06XQSEzS8SO5ltiaW2fMmN5C45OXtKE4rz7zlR/IOJLLBkJEV5UeSCtWECQQD5DrpzQP+SFZvWbkUwmARKR58AwskODzHZkNv8+PK1DOkUIjsHzJrt7ZeFF/gabWYjNpcmZeHOIC/u3+MfeglRAkEAsZzp0Hlwp0Xxq1j9gt2gYcieLnI+9zM0QSbfS0Jt1KffQBwo9rKYkB4RoSBmTMegGMNJCPPM+egUkpuKHnjAuwJBAKskDccpAPV3V9CvkWfk7f3E3WP+dX3tvwmL2z8oKk9zAa7OHtYrKMMaIqZYZCp0xlyoJJFELTghMg7pVf7JMyECQQCLB2zux2CrT1DQ86PdoGY+pK9NZBhtoCMzLJ51ZtZaM93JbUGTelF53k+mdJofV9O2DodI/q5goI4aBK6lE5ipAkBb2qqAdrWLalsLBE02MAKMA2QExO9bgTb/7e5XUWGPyuvxq9RdCWDKNMKKnpn1Dba0VQfCz800TFs+3rSiMlV9 -----END RSA PRIVATE KEY-----"; //Shitty random test key
+		var shit2 = new JSEncrypt();
+		shit2.setPrivateKey(shit);
+		var shit3 = shit2.sign(shit0, CryptoJS.SHA256, "sha256");
+		console.log(shit3);
+		console.log(signature);*/
+
 		if(action === null && signature === null){
 			isDataInUrl = false;
 			action = getCookie("pretix_autocart_action");
@@ -28,10 +36,9 @@ $(document).ready(function(){
 			if(action === null && signature === null) return;
 		}
 
-
 		var jse = new JSEncrypt();
 		jse.setPublicKey(data);
-		var result = jse.verify(action, signature, CryptoJS.SHA256);
+		var result = jse.verify(action, signature.replaceAll("-", "+").replaceAll("_", "/"), CryptoJS.SHA256);
 		if(!result){ console.log("Invalid signature detected!"); return; }
 
 		action = action.replaceAll(/[^a-zA-Z0-9\-\_\+\/]+/gm, ""); //Better sanitize to not destroy cookies :P
@@ -43,7 +50,7 @@ $(document).ready(function(){
 		}
 
 		var previousKeys = getCookie("pretix_autocart_previous"); //Get previously filled questions/cart positions. We give the chance to the user to edit their cart
-		previousKeys = (previousKeys === null || isDataInUrl) ? [] : previousKeys.split("@"); //Dumb way of splitting
+		previousKeys = (previousKeys === null || previousKeys === "" || isDataInUrl) ? [] : previousKeys.split("@"); //Dumb way of splitting
 
 		action = JSON.parse(atob(action));
 		actionIds = Object.keys(action);
@@ -62,6 +69,7 @@ $(document).ready(function(){
 
 					if(type === 'v'){
 						obj.value = value;
+						obj.focus();
 					} else {
 						value = value === '1';
 						if(obj.checked !== value) obj.click();
--- urlGeneratorExample.py
+++ urlGeneratorExample.py
@@ -26,20 +26,19 @@ def generateUrl(shopUrl: str, listAction: list, keyPassword: str = None) -> str:
         if(action.type == TYPE_VALUE):
             data[action.targetId] = action.type + str(action.value)
         else: # Checkmarks:
-            data[action.targetId] = action.type + ("1" if action.value else "0")
+            data[action.targetId] = action.type + ("1" if action.value else "0") #checkmarks are encoded as 0 or 1
 
-    print(json.dumps(data))
-    data = base64.urlsafe_b64encode(json.dumps(data).encode("utf-8"))
-    print(data)
+    data = base64.urlsafe_b64encode(json.dumps(data).encode("utf-8")).replace(b"=", b"")
 
     with open('priv-key.rsa', 'r') as f:
         k = RSA.importKey(f.read(),  passphrase=keyPassword)
 
     hash = SHA256.new(data)
-    signature = base64.urlsafe_b64encode(pkcs1_15.new(k).sign(hash)) #sign is made with the b64 of the json!
-    
+    signed = pkcs1_15.new(k).sign(hash)
+    signature = base64.urlsafe_b64encode(signed) #sign is made with the b64 of the json with the final = truncated! Sign has - instead of +, _ instead of / and trailing = truncated
+
     signature = signature.decode("utf-8").replace("=", "")
-    data = data.decode("utf-8").replace("=", "")
+    data = data.decode("utf-8")
 
     return f"{shopUrl}#a={data}&s={signature}"
 
@@ -47,8 +46,9 @@ def generateUrl(shopUrl: str, listAction: list, keyPassword: str = None) -> str:
 
 
 l = []
-l.append(Action("test", "asd", TYPE_VALUE))
-l.append(Action("kek", True, TYPE_BOOL))
+l.append(Action("item_1", 1, TYPE_VALUE))
+l.append(Action("cp_41_item_3", 1, TYPE_BOOL))
+l.append(Action("id_41-question_1", "non bossetti", TYPE_VALUE))
 
 shopUrl = "http://localhost:8000/suka/testPayment/"
 
